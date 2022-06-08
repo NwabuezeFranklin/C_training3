1b8f3417aae95122fe64a1973ea137e1d327fb98
Nwabueze Franklin <nwaframk2017@gmail.com>
2022-06-08 16:13:12 +0100

    Clarified a member

---
+++ ty.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int time = 60;
+    if (time == 70)
+    {
+        printf("Good morning");
+    }
+    else if( time == 60)
+    {
+        printf("Good day");
+    }
+    return 0;
+}
