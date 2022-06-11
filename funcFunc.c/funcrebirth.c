4c78d134f0da2b008ff7e62ca7d6f0ed46c7a019
Nwabueze Franklin <nwaframk2017@gmail.com>
2022-06-11 02:51:35 +0100

    No comments

---
+++ funcFunc.c/funcrebirth.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+//still on it
+// simple pointers
+int main()
+{
+    // int m = 28;
+    //int* ptr;
+    //int **pptr;
+    //ptr = &m;
+    //pptr = &ptr;
+    //*ptr = 23;
+    //**pptr = 21;
+    //printf("%d", m);
+  /*int *alay;
+  int array[] ={2,4,5,1};
+  alay = &array;
+  *alay = 23;
+  printf("%d\n", array[0]);
+  printf("%d\n", array[1]);
+  printf("%d\n", *alay);
+  printf("%d\n", *(alay+2));
+  printf("%d\n", alay+1);
+  printf("%d\n", alay+2);
+  */
+ 
+}
