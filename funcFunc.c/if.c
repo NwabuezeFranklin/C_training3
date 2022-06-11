16055b5c32cbade89a38069afefcab3ce3972917
Nwabueze Franklin <nwaframk2017@gmail.com>
2022-06-11 02:54:21 +0100

    to be updated :(

---
+++ funcFunc.c/if.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+//Retribution
+//No days off
+int main()
+{
+    int i = 4;
+
+    if(i == 4)
+    {
+        printf("this was correct");
+    }else if(i == 6)
+    {
+        printf("this was close");
+
+    }else if(i == 5)
+    {
+        printf("this was closer");
+
+    }else if
+}
