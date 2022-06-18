f94b4a13b69d16a6b31aa4e8909eb4f1d3457161
Nwabueze Franklin <nwaframk2017@gmail.com>
2022-06-18 01:02:41 +0100

    Some Clarifications

---
+++ funcFunc.c/Revisions/check.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+//Retribution
+int main()
+{
+    int a =3, b = 4, n;
+    char *r;
+    printf("Ener a symbol  \n");
+    scanf("%s", &r);
+    ( r == "*");
+    {
+        n = a * b;
+        printf(" %d ", n);
+    } if (r == "/")
+    {
+        n = a + b;
+        printf(" %d ", n);
+
+    } 
+}
